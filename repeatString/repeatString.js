129204eedd8e99978948fbbc6d767030dcde4f32
anthony-rk <anthonyrkowalski@gmail.com>
2020-01-23 20:06:35 -0800

    repeatString.js and repeatString.spec.js updated to pass test conditions

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(phrase, num) {
+    newPhrase = "";
+    
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    for (let i = 0; i < num; i++) {
+        newPhrase += phrase;
+    }
+    return newPhrase;
 }
 
 module.exports = repeatString
