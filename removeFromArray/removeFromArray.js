ab935f35e6a25171d6ff0440296f5d384dda193f
anthony-rk <anthonyrkowalski@gmail.com>
2020-01-23 20:45:17 -0800

    removeFromArray.js updated to pass test conditions

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let args = Array.from(arguments);
 
+    for (let j = 1; j < args.length; j++) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === args[j]) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 }
 
 module.exports = removeFromArray
