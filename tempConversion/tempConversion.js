7a11f9d71e710814f88b9f2b19afb14af52115bd
anthony-rk <anthonyrkowalski@gmail.com>
2020-01-23 21:21:04 -0800

    tempConversion.js updated to pass test conditions

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  let celcius = Math.round(((farenheit - 32) * (5/9)) * 10) / 10;
+  return celcius;
 }
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  let farenheit = Math.round(((celcius * 9/5) + 32) * 10) / 10;
+  return farenheit;
 }
 
 module.exports = {
