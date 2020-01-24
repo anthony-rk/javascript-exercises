166a026f5f4d19730935e3634719605fd6c210ae
anthony-rk <anthonyrkowalski@gmail.com>
2020-01-23 20:31:35 -0800

    reverseString.js updated to pass test conditions

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(phrase) {
+	// Check if input is valid
+	if (!phrase || phrase.length < 2 || typeof phrase != 'string') {
+		return "Not Valid";
+	}
 
+    const revArray = [];
+    for (let i = phrase.length - 1; i >= 0; i--) {
+        revArray.push(phrase[i]);
+    }
+    return revArray.join("");;
 }
 
 module.exports = reverseString
