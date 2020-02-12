69d3b4bd7c410637952d1a637cbc386d19057021
anthony-rk <anthonyrkowalski@gmail.com>
2020-02-11 20:24:51 -0800

    Add calculator.js exercise passing all calculator.spec.js jasmine tests

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,26 +1,42 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (sumArray) {
+	return sumArray.reduce((a, b) => a + b, 0);
 }
 
-function multiply () {
+function multiply (multArray) {
+	let multResult = multArray[0]; 
+	for (let i = 1; i < multArray.length; i++) {
+		multResult *= multArray[i];
+	}
+	return multResult;
+}
 	
+
+function power(a, b) {
+	return a ** b;
 }
 
-function power() {
-	
-}
+function factorial(input) {
+	let sum = 0;
 
-function factorial() {
-	
-}
+	if (input == 0 || input == 1) {
+		return 1;
+	} else {
+		sum = 1;
+		while (input > 0) {
+			sum *= input;
+			input--;
+		}
+	}
+	return sum;
+};
 
 module.exports = {
 	add,
