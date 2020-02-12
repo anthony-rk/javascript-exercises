24f5869645b11fcb7986ad5a9903de4da32451a0
anthony-rk <anthonyrkowalski@gmail.com>
2020-02-11 21:13:21 -0800

    Add palindromes.js file passing tests per the palindromes.spec.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function() {
+const palindromes = function(inputString) {
+    var regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s]/g;
+
+    let removePunctuation = function(string) {
+      return string.replace(regex, '');
+    }
+      
+    let reverseString = function(str) {
+        return str.split('').reverse().join('');
+    }
+
+    return removePunctuation(reverseString(inputString.toLowerCase())) == removePunctuation(inputString.toLowerCase()) ? true : false;
 
 }
 
