a542293a2e1e56f37d860a2354f98403cbad8368
anthony-rk <anthonyrkowalski@gmail.com>
2020-02-15 16:37:54 -0800

    Completed fibonacci.js function

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    // calculate the fibonacci array until the index
+    // return the final entry, aka the index
+    let fibonacciArray = [1, 1];
+    let nextInSequence = 0;
 
+    if (index == 0 || index == 1) {
+        return 1; 
+    } 
+    else if (index < 0) {
+        return "OOPS";
+    } 
+    else {
+        for (let i = 2; i < index; i++) {
+            nextInSequence = (fibonacciArray[i - 1] + fibonacciArray[i - 2]);
+            fibonacciArray.push(nextInSequence);
+        }   
+        return fibonacciArray[fibonacciArray.length - 1];
+    }
 }
 
 module.exports = fibonacci
